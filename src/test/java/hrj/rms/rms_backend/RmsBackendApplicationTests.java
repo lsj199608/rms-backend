0c8eab93666832ac76f8bfdc770832c311396f0e
이승재 <lsj199608@naver.com>
2026-01-19 21:15:22 +0900

    이승재 : 깡통 커밋

---
+++ src/test/java/hrj/rms/rms_backend/RmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hrj.rms.rms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
